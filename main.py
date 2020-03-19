3d35a3afab1d4b16edf3c77fd213da9556444952
rkpolo <39287781+rkpolo@users.noreply.github.com>
2020-03-19 23:01:32 +0800

    Created Interactive Page
    Able to produce model output on the same page.
    
    Next will work on creating multiple outputs, followed by making them clickable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,jsonify
 import pandas as pd 
 import gpt_2_simple as gpt2
 
@@ -19,7 +19,20 @@ def predict():
 
 	return render_template('result.html',prediction = my_prediction[0])
 
+@app.route('/interactive')
+def interactive_input():
+	return render_template('interactive.html')
 
+@app.route('/background_process')
+def background_process():
+	try:
+		lang = request.args.get('message', 0, type=str)
+		sess = gpt2.start_tf_sess()
+		gpt2.load_gpt2(sess, run_name='review_star_1_large')
+		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_large', length=50, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=1, return_as_list=True)
+		return jsonify(result = my_prediction[0])
+	except Exception as e:
+		return str(e)
 
 if __name__ == '__main__':
 	app.run(debug=True)

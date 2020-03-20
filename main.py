3da682e62088d12542066fc325510a6c8b8553a7
rkpolo <39287781+rkpolo@users.noreply.github.com>
2020-03-20 12:22:41 +0800

    Load 3 reviews at once
    Able to load 3 reviews at once.
    
    Next step is working on being able to click the reviews and send back to the text box

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
+#Original Basic code
 @app.route('/predict',methods=['POST'])
 def predict():
 	sess = gpt2.start_tf_sess()
@@ -19,18 +20,23 @@ def predict():
 
 	return render_template('result.html',prediction = my_prediction[0])
 
+#Reactive code
 @app.route('/interactive')
 def interactive_input():
-	return render_template('interactive.html')
+	return render_template('interactive.html', prediction = "Hello")
 
+#Reactive function that will enable the code to run 
 @app.route('/background_process')
 def background_process():
 	try:
 		lang = request.args.get('message', 0, type=str)
 		sess = gpt2.start_tf_sess()
 		gpt2.load_gpt2(sess, run_name='review_star_1_large')
-		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_large', length=50, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=1, return_as_list=True)
-		return jsonify(result = my_prediction[0])
+		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_large', length=50, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
+		print("First result: " + str(my_prediction[0]))
+		print("Second result: " + str(my_prediction[1]))
+		print("Third result: " + str(my_prediction[2]))
+		return jsonify(result1 = my_prediction[0], result2 = my_prediction[1], result3 = my_prediction[2])
 	except Exception as e:
 		return str(e)
 

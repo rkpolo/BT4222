70a369b8502c212d274f724db87bb31e677d311d
rkpolo <39287781+rkpolo@users.noreply.github.com>
2020-03-31 01:19:19 +0800

    MVP completed
    Added on the slide bar as part of the MVP product.
    
    Adding CSS through bootstrap now

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
 from flask import Flask,Response,render_template,url_for,request,jsonify
+from flask_bootstrap import Bootstrap
 import pandas as pd 
 import gpt_2_simple as gpt2
-from wtforms import TextField, Form
 import json
 
 
 app = Flask(__name__)
-
-#Tesing the autocomplete
-cities = ["Bratislava",
-          "Banská Bystrica",
-          "Prešov",
-          "Považská Bystrica",
-          "Žilina",
-          "Košice",
-          "Ružomberok",
-          "Zvolen",
-          "Poprad"]
-
-
-class SearchForm(Form):
-    autocomp = TextField('Insert City', id='city_autocomplete')
-
-
-#Original Basic code
-@app.route('/predict',methods=['POST'])
-def predict():
-	sess = gpt2.start_tf_sess()
-	gpt2.load_gpt2(sess, run_name='review_star_1_large')
-	if request.method == 'POST':
-		message = request.form['message']
-		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_large', length=50, prefix= message, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=1, return_as_list=True)
-
-	return render_template('result.html',prediction = my_prediction[0])
+Bootstrap(app)
 
 #Reactive code
 @app.route('/')
@@ -44,25 +18,18 @@ def interactive_input():
 def background_process():
 	try:
 		lang = request.args.get('message', 0, type=str)
+		complexity = request.args.get('complexity', 0, type=str)
+		complexity = float(complexity)
 		sess = gpt2.start_tf_sess()
 		gpt2.load_gpt2(sess, run_name='review_star_1_30000')
-		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_30000', length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
-		print("First result: " + str(my_prediction[0]))
-		print("Second result: " + str(my_prediction[1]))
-		print("Third result: " + str(my_prediction[2]))
-		return jsonify(result1 = my_prediction[0], result2 = my_prediction[1], result3 = my_prediction[2])
+		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_30000',temperature=complexity, length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
+		res1 = str(my_prediction[0]).replace('<|endoftext|>', '')
+		res2 = str(my_prediction[1]).replace('<|endoftext|>', '')
+		res3 = str(my_prediction[2]).replace('<|endoftext|>', '')
+		return jsonify(result1 = res1, result2 = res2, result3 = res3)
 	except Exception as e:
 		return str(e)
 
-@app.route('/_autocomplete', methods=['GET'])
-def autocomplete():
-    return Response(json.dumps(cities), mimetype='application/json')
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def index():
-    form = SearchForm(request.form)
-    return render_template("search.html", form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)

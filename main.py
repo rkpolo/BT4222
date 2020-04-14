a4e52fa9693c6c0f364b3e7869ba9d2d4d427ce5
rkpolo <39287781+rkpolo@users.noreply.github.com>
2020-04-14 19:16:16 +0800

    Completed version
    Have a main home page with all the other food pages customized with different headers

--- main.py
+++ main.py
@@ -8,25 +8,38 @@ import json
 app = Flask(__name__)
 Bootstrap(app)
 
-#Reactive code
+#Main Page
 @app.route('/')
 def interactive_input():
-	return render_template('interactive.html')
+	return render_template('main.html')
 
-@app.route('/bootstrap')
-def bootstrap():
-	return render_template('bootstrap.html')
+#Creating the different routes
+@app.route('/food_1_star')
+def food_1_star():
+	return render_template('food_1.html')
+
+@app.route('/food_5_star')
+def food_5_star():
+	return render_template('food_5.html')
+
+@app.route('/general_1_star')
+def general_1_star():
+	return render_template('general_1.html')
+
+@app.route('/general_5_star')
+def general_5_star():
+	return render_template('general_5.html')
 
 #Reactive function that will enable the code to run 
-@app.route('/background_process')
-def background_process():
+@app.route('/food_1')
+def food_1():
 	try:
 		lang = request.args.get('message', 0, type=str)
 		complexity = request.args.get('complexity', 0, type=str)
 		complexity = float(complexity)
 		sess = gpt2.start_tf_sess()
 		gpt2.load_gpt2(sess, run_name='review_star_1_30000')
-		my_prediction = gpt2.generate(sess, run_name= 'review_star_1_30000',temperature=complexity, length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
+		my_prediction = gpt2.generate(sess, run_name= 'food_1_star_large',temperature=complexity, length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
 		res1 = str(my_prediction[0]).replace('<|endoftext|>', '')
 		res2 = str(my_prediction[1]).replace('<|endoftext|>', '')
 		res3 = str(my_prediction[2]).replace('<|endoftext|>', '')
@@ -34,6 +47,56 @@ def background_process():
 	except Exception as e:
 		return str(e)
 
+#Reactive function that will enable the code to run 
+@app.route('/food_5')
+def food_5():
+	try:
+		lang = request.args.get('message', 0, type=str)
+		complexity = request.args.get('complexity', 0, type=str)
+		complexity = float(complexity)
+		sess = gpt2.start_tf_sess()
+		gpt2.load_gpt2(sess, run_name='review_star_1_30000')
+		my_prediction = gpt2.generate(sess, run_name= 'food_5_star_large',temperature=complexity, length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
+		res1 = str(my_prediction[0]).replace('<|endoftext|>', '')
+		res2 = str(my_prediction[1]).replace('<|endoftext|>', '')
+		res3 = str(my_prediction[2]).replace('<|endoftext|>', '')
+		return jsonify(result1 = res1, result2 = res2, result3 = res3)
+	except Exception as e:
+		return str(e)
+
+#Reactive function that will enable the code to run 
+@app.route('/general_1')
+def general_1():
+	try:
+		lang = request.args.get('message', 0, type=str)
+		complexity = request.args.get('complexity', 0, type=str)
+		complexity = float(complexity)
+		sess = gpt2.start_tf_sess()
+		gpt2.load_gpt2(sess, run_name='review_star_1_30000')
+		my_prediction = gpt2.generate(sess, run_name= 'general_1_star_large',temperature=complexity, length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
+		res1 = str(my_prediction[0]).replace('<|endoftext|>', '')
+		res2 = str(my_prediction[1]).replace('<|endoftext|>', '')
+		res3 = str(my_prediction[2]).replace('<|endoftext|>', '')
+		return jsonify(result1 = res1, result2 = res2, result3 = res3)
+	except Exception as e:
+		return str(e)
+
+#Reactive function that will enable the code to run 
+@app.route('/general_5')
+def general_5():
+	try:
+		lang = request.args.get('message', 0, type=str)
+		complexity = request.args.get('complexity', 0, type=str)
+		complexity = float(complexity)
+		sess = gpt2.start_tf_sess()
+		gpt2.load_gpt2(sess, run_name='review_star_1_30000')
+		my_prediction = gpt2.generate(sess, run_name= 'general_5_star_large',temperature=complexity, length=15, prefix= lang, sample_delim = '<|endoftext|>', include_prefix=False, nsamples=3, return_as_list=True)
+		res1 = str(my_prediction[0]).replace('<|endoftext|>', '')
+		res2 = str(my_prediction[1]).replace('<|endoftext|>', '')
+		res3 = str(my_prediction[2]).replace('<|endoftext|>', '')
+		return jsonify(result1 = res1, result2 = res2, result3 = res3)
+	except Exception as e:
+		return str(e)
 
 if __name__ == '__main__':
 	app.run(debug=True)
